c9ed86d6bdb0ba7a9241e219150c65b4c5d1b39c
Rob Zyskowski <zyskowski.rob@gmail.com>
2026-05-28 14:17:31 +0200

    fix for python 3

--- setup.py
+++ setup.py
@@ -1,29 +1,26 @@
 #!/usr/bin/env python
-
 import os, sys
 script = os.path.basename(__file__)
-source_dir = os.path.abspath('./' + os.path.dirname(__file__) + "/")
-
+source_dir = os.path.dirname(os.path.abspath(__file__))
 for f in os.listdir(source_dir):
     if f in [script, '.git', '.gitignore']:
         continue
-
     destination = os.path.expanduser('~') + "/" + f
     source = source_dir + "/" + f
     try:
         os.symlink(source, destination)
-        print "\033[36;1m" + source + "\033[0m ->", destination
+        print("\033[36;1m" + source + "\033[0m ->", destination)
     except OSError as e:
         if e.errno == 17:
             try:
-                if os.readlink(source) == destination:
-                    print source, "is already linked"
+                if os.readlink(destination) == source:
+                    print(source, "is already linked")
                 else:
-                    print source, "\033[31;1mexists\033[0m (symlink)"
+                    print(source, "\033[31;1mexists\033[0m (symlink)")
             except OSError as e:
                 if e.errno == 22:
-                    print source, "\033[31;1mexists\033[0m"
+                    print(source, "\033[31;1mexists\033[0m")
                 else:
-                    print e
+                    print(e)
         else:
-            print e
+            print(e)
